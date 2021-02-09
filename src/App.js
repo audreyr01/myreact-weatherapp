48e34dc75c75046880b7ff22e52afaa008c897d3
audreyr01 <75100931+audreyr01@users.noreply.github.com>
2021-02-08 19:57:44 -0800

    Added Footer
    Added GitHub and LinkedIn url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React from "react";
+
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        Coded by
+        <a href="https://www.linkedin.com/in/robertsonmba" target="_blank">
+          {" "}
+          Audrey Robertson{" "}
+        </a>
+        <br />
+        Open Sourced on{" "}
+        <a
+          href="https://github.com/audreyr01/myreact-weatherapp"
+          target="_blank"
+        >
+          GitHub{" "}
+        </a>{" "}
+      </footer>
+    </div>
+  );
 }
